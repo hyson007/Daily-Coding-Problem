c4c743ef9fe1c7d9d07e401562ade8405d241cd8
hyson007 <36887937+hyson007@users.noreply.github.com>
2020-08-09 16:49:45 +0800

    Update 173.py

--- 173.py
+++ 173.py
@@ -1,3 +1,28 @@
+'''
+Write a function to flatten a nested dictionary. Namespace the keys with a period.
+
+For example, given the following dictionary:
+
+{
+    "key": 3,
+    "foo": {
+        "a": 5,
+        "bar": {
+            "baz": 8
+        }
+    }
+}
+it should become:
+
+{
+    "key": 3,
+    "foo.a": 5,
+    "foo.bar.baz": 8
+}
+'''
+
+
+
 my_dict = {
     "key": 3,
     "foo": {
